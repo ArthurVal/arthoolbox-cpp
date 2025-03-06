02c15903e418bc88f41e989f97babe86545f42a1
Arthur Valiente <valiente.arthur@gmail.com>
2025-03-06 10:56:24 +0100

    Add overload and type_traits

---
+++ include/atb-cpp/overload.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "atb-cpp/type_traits.hpp"
+
+namespace atb {
+
+/// Implementation of the overloads pattern
+///(see https://en.cppreference.com/w/cpp/utility/variant/visit)
+template <typename... Matchers>
+struct Overload : Matchers... {
+  using Matchers::operator()...;
+};
+
+/// CTAD (Not needed as of c++20)
+template <typename... Matchers>
+Overload(Matchers...) -> Overload<Matchers...>;
+
+/// Implementation of the overloads pattern
+///(see https://en.cppreference.com/w/cpp/utility/variant/visit)
+template <typename... Matchers>
+struct StrictOverload : Matchers... {
+  using Matchers::operator()...;
+
+  /// Default overload use to triggers a compilation error when an operator<T>()
+  /// is missing
+  template <typename... T>
+  constexpr auto operator()(T...) const {
+    static_assert(details::AlwaysFalse_v<T...>,
+                  "operator()(T...) is not implemented");
+  }
+};
+
+/// CTAD (Not needed as of c++20)
+template <typename... Matchers>
+StrictOverload(Matchers...) -> StrictOverload<Matchers...>;
+
+}  // namespace atb
---
+++ include/atb-cpp/type_traits.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <type_traits>  // false_type
+
+namespace atb::details {
+
+/// Always evaluates to false
+template <typename...>
+struct AlwaysFalse : std::false_type {};
+
+template <typename... T>
+constexpr bool AlwaysFalse_v = AlwaysFalse<T...>::value;
+
+/// Always evaluates to true
+template <typename...>
+struct AlwaysTrue : std::true_type {};
+
+template <typename... T>
+constexpr bool AlwaysTrue_v = AlwaysTrue<T...>::value;
+
+/// Remove reference, const and volatile qualifiers.
+template <class T>
+struct RemoveCVRef {
+  using type = std::remove_cv_t<std::remove_reference_t<T>>;
+};
+
+template <class T>
+using RemoveCVRef_t = typename RemoveCVRef<T>::type;
+
+}  // namespace atb::details
