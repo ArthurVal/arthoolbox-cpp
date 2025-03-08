21e2eb1682db35b28735a729d8da62f68262c942
Arthur Valiente <valiente.arthur@gmail.com>
2025-03-08 09:31:30 +0100

    Remove range.hpp and use iterator.hpp instead

---
+++ include/atb-cpp/iterator.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <cassert>
+#include <iterator>  // iterator_traits
+#include <optional>
+#include <utility> //pair
+
+namespace atb {
+
+template <class It>
+constexpr auto Slice(It first, It last, std::size_t begin,
+                     std::optional<std::size_t> new_size = std::nullopt) noexcept
+    -> std::pair<It, It> {
+  const auto dist = std::distance(first, last);
+  if(dist > 0) {
+    const auto initial_size = static_cast<std::size_t>(dist);
+
+    // Clamp begin to the initial_size
+    begin = std::min(begin, initial_size);
+    std::advance(first, begin);
+  
+    if (new_size.has_value() and (first != last)) {
+      last = std::next(first, std::min(*new_size, (initial_size - begin)));
+    }
+  }
+
+  return std::make_pair(first, last);
+}
+
+
+}  // namespace atb
