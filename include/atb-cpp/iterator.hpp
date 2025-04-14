dd1ac0f950cf4a047d042fbec7da027052b9cd8e
Arthur Valiente <valiente.arthur@gmail.com>
2025-04-14 14:42:50 +0200

    Apply format to iterator.hpp

--- include/atb-cpp/iterator.hpp
+++ include/atb-cpp/iterator.hpp
@@ -3,29 +3,29 @@
 #include <cassert>
 #include <iterator>  // iterator_traits
 #include <optional>
-#include <utility> //pair
+#include <utility>  //pair
 
 namespace atb {
 
 template <class It>
 constexpr auto Slice(It first, It last, std::size_t begin,
-                     std::optional<std::size_t> new_size = std::nullopt) noexcept
-    -> std::pair<It, It> {
+                     std::optional<std::size_t> new_size =
+                         std::nullopt) noexcept -> std::pair<It, It> {
   const auto dist = std::distance(first, last);
-  if(dist > 0) {
+  if (dist > 0) {
     const auto initial_size = static_cast<std::size_t>(dist);
 
     // Clamp begin to the initial_size
     begin = std::min(begin, initial_size);
     std::advance(first, begin);
-  
+
     if (new_size.has_value() and (first != last)) {
-      last = std::next(first, std::min(*new_size, (initial_size - begin)));
+      const auto remaining = (initial_size - begin);
+      last = std::next(first, std::min(*new_size, remaining));
     }
   }
 
   return std::make_pair(first, last);
 }
-
 
 }  // namespace atb
