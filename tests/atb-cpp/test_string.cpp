55c8fac8f0ebd2a0008271df1658c782b7d65b10
ArthurVal <4202073+ArthurVal@users.noreply.github.com>
2026-06-30 10:30:40 +0200

    Add empty test_string

---
+++ tests/atb-cpp/test_string.cpp
@@ -0,0 +1,13 @@
+#include "atb-cpp/string.hpp"
+#include "gtest/gtest.h"
+
+using namespace std::literals::string_view_literals;
+
+namespace atb {
+namespace {
+
+TEST(AtbStringTest, StrSize) {}
+
+}  // namespace
+
+}  // namespace atb
