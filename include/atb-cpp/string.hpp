d87288dcc4a71aea15a2f164ffdc5c25b39a6e0f
ArthurVal <4202073+ArthurVal@users.noreply.github.com>
2026-06-30 11:18:22 +0200

    Remove single StrCopy function (Add StrCopyUnsafe) + Add tests

--- include/atb-cpp/string.hpp
+++ include/atb-cpp/string.hpp
@@ -4,7 +4,6 @@
 #include <cassert>    // assert
 #include <cstdint>    // std::uintmax_t
 #include <initializer_list>
-#include <optional>
 #include <string>
 #include <string_view>
 
@@ -22,50 +21,40 @@ constexpr auto StrSize(std::initializer_list<std::string_view> strings) noexcept
 }
 
 /**
- *   @brief Options forwarded to the StrCopy function
- */
-struct StrCopy_Opt {
-  /// Size of the destination range (in bytes)
-  std::optional<std::size_t> d_size = std::nullopt;
-
-  /// Indicates if, when the dest range overflows, you still want to partially
-  /// copy the last string in it (i.e. dest range will contain a cropped string
-  /// in it)
-  bool crop = false;
-};
-
-/**
  *  @brief Copy the range of \a strings into \a d_first
  *
  *  @param[in] strings List of strings we wish to copy
  *  @param[in] d_first The destination char buffer
- *  @param[in] opt Options use to specify the copy policy.
+ *  @param[in] d_size The destination char buffer size
+ *  @param[in] crop Indicates if we want (or not) to crop the last string
+ *                  written into \a d_first when the destination buffer would
+ *                  overflows. Default to false (no crop).
  *  @return One past the last byte written into
  */
 constexpr auto StrCopy(std::initializer_list<std::string_view> strings,
-                       char* d_first, StrCopy_Opt opt = {}) noexcept -> char* {
-  if (opt.d_size.has_value()) {
-    // Safe copy
-    std::size_t d_size = std::move(*opt.d_size);
-    for (auto str : strings) {
-      if (d_size >= str.size()) {
-        d_first = std::copy_n(str.data(), str.size(), d_first);
-        d_size -= str.size();
-      } else {
-        if (opt.crop && (d_size > 0)) {
-          d_first = std::copy_n(str.data(), d_size, d_first);
-          d_size = 0;
-        }
-        break;
-      }
-    }
-  } else {
-    // Unsafe Copy
-    for (auto str : strings) {
+                       char* d_first, std::size_t d_size,
+                       bool crop = false) noexcept -> char* {
+  for (auto str : strings) {
+    if (d_size >= str.size()) {
       d_first = std::copy_n(str.data(), str.size(), d_first);
+      d_size -= str.size();
+    } else {
+      if (crop && (d_size > 0)) {
+        d_first = std::copy_n(str.data(), d_size, d_first);
+        d_size = 0;
+      }
+      break;
     }
   }
 
+  return d_first;
+}
+
+constexpr auto StrCopyUnsafe(std::initializer_list<std::string_view> strings,
+                             char* d_first) noexcept -> char* {
+  for (auto str : strings) {
+    d_first = std::copy_n(str.data(), str.size(), d_first);
+  }
   return d_first;
 }
 
@@ -84,8 +73,8 @@ constexpr auto StrCopy(std::initializer_list<std::string_view> strings,
  *        it for each new '.append()' or 'operator+=()' call at the cost of
  *        iterating over the strings 2 times
  */
-inline auto StrAppend(std::initializer_list<std::string_view> strings,
-                      std::string& d_str) -> std::size_t {
+constexpr auto StrAppend(std::initializer_list<std::string_view> strings,
+                         std::string& d_str) -> std::size_t {
   const std::uintmax_t old_size = d_str.size();
   const std::uintmax_t added = StrSize(strings);
   const std::uintmax_t new_size = old_size + added;
@@ -95,7 +84,7 @@ inline auto StrAppend(std::initializer_list<std::string_view> strings,
   // WARNING: bad_alloc
   d_str.resize(new_size);
 
-  StrCopy(std::move(strings), std::addressof(d_str[old_size]));
+  StrCopyUnsafe(std::move(strings), std::addressof(d_str[old_size]));
   return static_cast<std::size_t>(added);
 }
 
--- tests/atb-cpp/test_string.cpp
+++ tests/atb-cpp/test_string.cpp
@@ -1,3 +1,8 @@
+#include <algorithm>
+#include <array>
+#include <cstddef>
+#include <iterator>
+
 #include "atb-cpp/string.hpp"
 #include "gtest/gtest.h"
 
@@ -6,10 +11,105 @@ using namespace std::literals::string_view_literals;
 namespace atb {
 namespace {
 
+constexpr auto foo = "foo"sv;
+constexpr auto coucou = "Coucou"sv;
+constexpr auto sep = " "sv;
+constexpr auto chocolatine = "Chocolatine"sv;
+
 TEST(AtbStringTest, StrSize) {
-  EXPECT_EQ(0, StrSize({}));
-  EXPECT_EQ(3, StrSize({"foo"sv}));
-  EXPECT_EQ(10, StrSize({"abc", "def", "ghij"}));
+  EXPECT_EQ(StrSize({}), 0);
+  EXPECT_EQ(StrSize({foo}), foo.size());
+  EXPECT_EQ(StrSize({"0123", "456", "7", "89"}), 10);
+}
+
+TEST(AtbStringTest, StrCopyUnsafe) {
+  std::array<char, 256> buffer;
+  auto begin = std::begin(buffer);
+  auto end = std::end(buffer);
+  std::fill(begin, end, '\0');
+
+  auto res = StrCopyUnsafe({}, begin);
+  EXPECT_EQ(res, begin);
+
+  res = StrCopyUnsafe({coucou}, begin);
+  EXPECT_EQ(res, std::next(begin, coucou.size()));
+  EXPECT_EQ(coucou, std::string_view(begin, coucou.size()));
+
+  res = StrCopyUnsafe({chocolatine}, begin);
+  EXPECT_EQ(res, std::next(begin, chocolatine.size()));
+  EXPECT_EQ(chocolatine, std::string_view(begin, chocolatine.size()));
+
+  // Not using begin
+  res = StrCopyUnsafe({coucou}, res);
+  EXPECT_EQ(res, std::next(begin, chocolatine.size() + coucou.size()));
+  EXPECT_EQ("ChocolatineCoucou",
+            std::string_view(begin, chocolatine.size() + coucou.size()));
+
+  std::fill(begin, end, '\0');
+
+  // Multiple strings
+  res = StrCopyUnsafe({coucou, sep, chocolatine}, begin);
+  EXPECT_EQ(res,
+            std::next(begin, coucou.size() + sep.size() + chocolatine.size()));
+  EXPECT_EQ(
+      "Coucou Chocolatine",
+      std::string_view(begin, coucou.size() + sep.size() + chocolatine.size()));
+}
+
+TEST(AtbStringTest, StrCopy) {
+  std::array<char, 20> buffer;
+  auto begin = std::begin(buffer);
+  auto end = std::end(buffer);
+
+  std::fill(begin, end, '\0');
+  auto res = StrCopy({}, begin, buffer.size());
+  EXPECT_EQ(res, begin);
+
+  res = StrCopy({coucou}, begin, buffer.size());
+  EXPECT_EQ(res, std::next(begin, coucou.size()));
+  EXPECT_EQ(coucou, std::string_view(begin, coucou.size()));
+
+  std::fill(begin, end, '\0');
+  res = StrCopy({chocolatine}, begin, buffer.size());
+  EXPECT_EQ(res, std::next(begin, chocolatine.size()));
+  EXPECT_EQ(chocolatine, std::string_view(begin, chocolatine.size()));
+
+  // Not using begin
+  res =
+      StrCopy({coucou}, res, static_cast<std::size_t>(std::distance(res, end)));
+  EXPECT_EQ(res, std::next(begin, chocolatine.size() + coucou.size()));
+  EXPECT_EQ("ChocolatineCoucou",
+            std::string_view(begin, chocolatine.size() + coucou.size()));
+
+  // Multiple strings
+  std::fill(begin, end, '\0');
+  res = StrCopy({coucou, sep, chocolatine}, begin, buffer.size());
+  EXPECT_EQ(res,
+            std::next(begin, coucou.size() + sep.size() + chocolatine.size()));
+  EXPECT_EQ("Coucou Chocolatine",
+            std::string_view(
+                begin, static_cast<std::size_t>(std::distance(begin, res))));
+
+  // Overflows - No crop
+  std::fill(begin, end, '\0');
+  res = StrCopy({coucou, sep, chocolatine, chocolatine}, begin, buffer.size());
+  EXPECT_NE(res, std::next(begin, coucou.size() + sep.size() +
+                                      2 * chocolatine.size()));
+  EXPECT_EQ(res,
+            std::next(begin, coucou.size() + sep.size() + chocolatine.size()));
+
+  EXPECT_EQ("Coucou Chocolatine",
+            std::string_view(
+                begin, static_cast<std::size_t>(std::distance(begin, res))));
+
+  // Overflows - crop
+  std::fill(begin, end, '\0');
+  res = StrCopy({coucou, sep, chocolatine, chocolatine}, begin, buffer.size(),
+                true);
+  EXPECT_EQ(res, end);
+  EXPECT_EQ("Coucou ChocolatineCh",
+            std::string_view(
+                begin, static_cast<std::size_t>(std::distance(begin, res))));
 }
 
 }  // namespace
