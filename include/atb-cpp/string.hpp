4d2d0edb9ce258b53fc3796bf9dc24f6a7bbfac2
ArthurVal <4202073+ArthurVal@users.noreply.github.com>
2026-06-30 10:33:02 +0200

    Refactor string.hpp

--- include/atb-cpp/string.hpp
+++ include/atb-cpp/string.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
 #include <algorithm>  // std::copy_n
-#include <cassert>
-#include <cstdint>  // std::uintmax_t
+#include <cassert>    // assert
+#include <cstdint>    // std::uintmax_t
 #include <initializer_list>
+#include <optional>
 #include <string>
 #include <string_view>
 
@@ -21,63 +22,58 @@ constexpr auto StrSize(std::initializer_list<std::string_view> strings) noexcept
 }
 
 /**
- *  @brief Unsafely Copy the range of \a strings into \a d_first
+ *   @brief Options forwarded to the StrCopy function
+ */
+struct StrCopy_Opt {
+  /// Size of the destination range (in bytes)
+  std::optional<std::size_t> d_size = std::nullopt;
+
+  /// Indicates if, when the dest range overflows, you still want to partially
+  /// copy the last string in it (i.e. dest range will contain a cropped string
+  /// in it)
+  bool crop = false;
+};
+
+/**
+ *  @brief Copy the range of \a strings into \a d_first
  *
- *  @param[in] d_first The destination char buffer
  *  @param[in] strings List of strings we wish to copy
- *
+ *  @param[in] d_first The destination char buffer
+ *  @param[in] opt Options use to specify the copy policy.
  *  @return One past the last byte written into
  */
-constexpr auto StrCopyInto(
-    char* d_first,
-    std::initializer_list<std::string_view> strings) noexcept -> char* {
-  for (auto str : strings) {
-    d_first = std::copy_n(str.data(), str.size(), d_first);
+constexpr auto StrCopy(std::initializer_list<std::string_view> strings,
+                       char* d_first, StrCopy_Opt opt = {}) noexcept -> char* {
+  if (opt.d_size.has_value()) {
+    // Safe copy
+    std::size_t d_size = std::move(*opt.d_size);
+    for (auto str : strings) {
+      if (d_size >= str.size()) {
+        d_first = std::copy_n(str.data(), str.size(), d_first);
+        d_size -= str.size();
+      } else {
+        if (opt.crop && (d_size > 0)) {
+          d_first = std::copy_n(str.data(), d_size, d_first);
+          d_size = 0;
+        }
+        break;
+      }
+    }
+  } else {
+    // Unsafe Copy
+    for (auto str : strings) {
+      d_first = std::copy_n(str.data(), str.size(), d_first);
+    }
   }
 
   return d_first;
 }
 
-struct FixedSizedBuffer {
-  char* begin;
-  std::size_t size;
-};
-
 /**
- *  @brief Safely Copy the range of \a strings into \a dest
+ *  @brief Append the list of strings into \a d_str by doing only one resize.
  *
- *  @param[in] dest A bounded destination char span
  *  @param[in] strings List of strings we wish to copy
- *  @param[in] crop Set to true when, if the output dest would overflow, you
- *                  still want to include an incomplete/cropped version of the
- *                  last string that would have overflowed the buffer
- *
- *  @return The remaining buffer after copy
- */
-constexpr auto StrCopyInto(FixedSizedBuffer dest,
-                           std::initializer_list<std::string_view> strings,
-                           bool crop = false) noexcept -> FixedSizedBuffer {
-  for (auto str : strings) {
-    if (dest.size >= str.size()) {
-      dest.begin = std::copy_n(str.data(), str.size(), dest.begin);
-      dest.size -= str.size();
-    } else {
-      if (crop && (dest.size > 0)) {
-        dest.begin = std::copy_n(str.data(), dest.size, dest.begin);
-        dest.size = 0;
-      }
-      break;
-    }
-  }
-
-  return dest;
-}
-
-/**
- *  @brief Append the list of strings into \a out by doing only one resize.
- *
- *  @param[out] out Destination string we wish to append into
- *  @param[in] strings List of strings we wish to copy
+ *  @param[out] d_str Destination string we wish to append into
  *
  *  @pre The number of bytes contained within strings fit into out
  *       (StrSize(strings) <= out.max_size())
@@ -88,19 +84,18 @@ constexpr auto StrCopyInto(FixedSizedBuffer dest,
  *        it for each new '.append()' or 'operator+=()' call at the cost of
  *        iterating over the strings 2 times
  */
-constexpr auto StrAppendTo(std::string& out,
-                           std::initializer_list<std::string_view> strings)
-    -> std::size_t {
-  const std::uintmax_t old_size = out.size();
+inline auto StrAppend(std::initializer_list<std::string_view> strings,
+                      std::string& d_str) -> std::size_t {
+  const std::uintmax_t old_size = d_str.size();
   const std::uintmax_t added = StrSize(strings);
   const std::uintmax_t new_size = old_size + added;
 
-  assert(new_size > out.max_size());
+  assert(new_size <= d_str.max_size());
 
   // WARNING: bad_alloc
-  out.resize(new_size);
+  d_str.resize(new_size);
 
-  StrCopyInto(std::addressof(out[old_size]), std::move(strings));
+  StrCopy(std::move(strings), std::addressof(d_str[old_size]));
   return static_cast<std::size_t>(added);
 }
 
@@ -114,11 +109,11 @@ constexpr auto StrAppendTo(std::string& out,
  *        it for each new '.append()' or 'operator+=()' call at the cost of
  *        iterating over the strings 2 times
  */
-inline auto StrConcat(std::initializer_list<std::string_view> strings)
+inline auto StrCat(std::initializer_list<std::string_view> strings)
     -> std::string {
-  std::string out;
-  StrAppendTo(out, strings);
-  return out;
+  std::string str;
+  StrAppend(strings, str);
+  return str;
 }
 
 }  // namespace atb
