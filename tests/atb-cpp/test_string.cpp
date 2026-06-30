e9840518a5b1ecdbc691f7d2ee77bfe2d4590ed0
ArthurVal <4202073+ArthurVal@users.noreply.github.com>
2026-06-30 10:33:13 +0200

    Add StrSize test

--- tests/atb-cpp/test_string.cpp
+++ tests/atb-cpp/test_string.cpp
@@ -6,7 +6,11 @@ using namespace std::literals::string_view_literals;
 namespace atb {
 namespace {
 
-TEST(AtbStringTest, StrSize) {}
+TEST(AtbStringTest, StrSize) {
+  EXPECT_EQ(0, StrSize({}));
+  EXPECT_EQ(3, StrSize({"foo"sv}));
+  EXPECT_EQ(10, StrSize({"abc", "def", "ghij"}));
+}
 
 }  // namespace
 
