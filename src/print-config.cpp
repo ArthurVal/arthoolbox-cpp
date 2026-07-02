ae94c8c37d96500e16360a172ec1c2f8e583fb16
ArthurVal <4202073+ArthurVal@users.noreply.github.com>
2026-07-02 09:32:06 +0200

    Fix missing print-config update

--- src/print-config.cpp
+++ src/print-config.cpp
@@ -5,10 +5,8 @@
 int main() {
   std::printf(
       "atb-cpp config:\n"
-      "- Build type  = %s\n"
-      "- Version     = %s\n"
-      "- Description = %s\n",
-      ATB_CPP_BUILD_TYPE, ATB_CPP_VERSION_STR, ATB_CPP_DESCRIPTION);
+      "- Version     = %s\n",
+      ATB_CPP_VERSION_STR);
 
   return 0;
 }
